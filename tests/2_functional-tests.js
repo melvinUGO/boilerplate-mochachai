74933ec436f26d1ef492d4de42707819b91283aa
Melvin Ugochukwu <ugochukwumelvin@gmail.com>
2023-10-15 15:41:20 -1100

    'FT 4

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -59,7 +59,7 @@ suite("Functional Tests", function () {
 
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.body.name, "Cristoforo");
+          assert.equal(res.body.name, "Giovanni");
           assert.equal(res.body.surname, "da Verrazzano");
           assert.equal(res.type, "application/json");
           done();
